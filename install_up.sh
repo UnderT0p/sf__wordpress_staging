8b34494f678221db5c1909b5b9a7a9c4308ff2b8
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-06-24 12:34:08 +0300

    Update install_up.sh

--- install_up.sh
+++ install_up.sh
@@ -19,6 +19,7 @@ sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/dock
 sudo yum install docker-ce docker-ce-cli containerd.io -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo systemctl start docker
 bash ~/sf__wordpress_staging/wp_up.sh
 
