2cf26f182a322c59aacff03cdc736716debfdb60
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-06-24 14:17:48 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source ~/sf__wordpress_staging/user.txt
-ssh $user if grep -q Debian /etc/issue then sudo apt install git elif grep -q CentOS  /etc/os-release then  sudo dnf install git-all
+ssh $user if grep -q Debian /etc/issue then sudo apt install git elif grep -q CentOS  /etc/os-release then  sudo dnf install git-all fi
 ssh $user git clone https://github.com/UnderT0p/sf__wordpress_experimental
 ssh $user bash ~/sf__wordpress_experimental/install_up.sh
