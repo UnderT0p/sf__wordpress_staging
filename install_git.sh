b6d6c9f13914df8be6d279bd8aa1030f64eaf6b6
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-06-24 14:51:06 +0300

    Update install_git.sh

--- install_git.sh
+++ install_git.sh
@@ -1,9 +1,11 @@
 if grep -q Debian /etc/issue 
 then 
+sudo apt-get update
 sudo apt-get install git -y
 elif grep -q CentOS  /etc/os-release 
 then  sudo dnf install git-all -y
 elif grep -q Ubuntu  /etc/os-release 
 then 
+sudo apt-get update
 sudo apt-get install git-all -y
 fi
