885954bc42c3f9674d38d9a3d908b2b8334f5ffb
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-06-24 14:08:27 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ~/sf__wordpress_staging/user.txt
 ssh $user git clone https://github.com/UnderT0p/sf__wordpress_experimental
-ssh $user bash ~/sf__wordpress_staging/install_up.sh
+ssh $user bash ~/sf__wordpress_experimental/install_up.sh
