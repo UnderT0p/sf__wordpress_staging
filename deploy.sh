7cdb4452d822bccfc77fc0dc7e697dde06c78f21
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-06-24 12:44:24 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,3 @@ source ~/sf__wordpress_staging/user.txt
 ssh $user mkdir ~/sf__wordpress_staging
 scp -r ~/sf__wordpress_staging/* $user:~/sf__wordpress_staging/
 ssh $user bash ~/sf__wordpress_staging/install_up.sh
-ssh $user sudo rm -rf ~/sf__wordpress_staging
